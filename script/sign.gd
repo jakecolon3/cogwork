6cdcfb9c478e69d43d8f4adb3b45f4d814084ee6
jakecolon3 <minecrafterxxl3309@gmail.com>
2026-07-10 19:42:28 +0200

    better sign

--- script/sign.gd
+++ script/sign.gd
@@ -1,12 +1,44 @@
 extends "res://script/interactable.gd"
 
+
 @export var text: String
+var scrolling: bool
+var text_index: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
     super._ready()
-    if text:
-        $Label.text = text
+    $Label.visible = false
+    if $Label.text:
+        text = $Label.text
+        $Label.text = ""
+    scrolling = false
+
+func skip_scroll() -> void:
+    $Label.text = text
+    scrolling = false
+    $Timer.stop()
+
 
 func _interact(interactor: Player) -> void:
-    visible = !visible
+    if scrolling:
+        skip_scroll()
+    else:
+        $Label.visible = !$Label.visible
+        if $Label.visible:
+            $Timer.start()
+            scrolling = true
+        else:
+            text_index = 0
+            $Label.text = ""
+
+
+# scrolls characters
+func _on_timer_timeout() -> void:
+    if text_index >= text.length():
+        $Timer.stop()
+        scrolling = false
+        return
+    text_index += 1
+    $Label.text = text.substr(0, text_index)
+    $Timer.start()
