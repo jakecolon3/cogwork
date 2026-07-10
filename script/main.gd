49651f5a518762227bea47aedfdbef9654269d40
jakecolon3 <minecrafterxxl3309@gmail.com>
2026-07-10 22:41:11 +0200

    level selecting in editor

--- script/main.gd
+++ script/main.gd
@@ -1,16 +1,20 @@
 extends Node2D
 
-@export var level: Level
+@export var level_name: String
 
 # TODO: better way to set starting position
 func _ready() -> void:
-    if not level:
+    if not level_name:
         var spawn_location : Vector2 = $TestLevel.spawn_location.position
         $Player.set_deferred("position", spawn_location)
         $Player.respawn_location = spawn_location
         get_tree().paused = true
     else:
         $TestLevel.free()
+        var level_scene := load("res://scenes/levels/%s.tscn" % level_name)
+        assert(level_scene, "a zi hai sbagliato il nome del livello")
+        var level : Level = level_scene.instantiate()
+        add_child(level)
         var spawn_location : Vector2 = level.spawn_location.position
         $Player.set_deferred("position", spawn_location)
         $Player.respawn_location = spawn_location
